8efab714b2560a0773bc94af01c3bb0da38e50d5
Alex Garrett-Smith <alex@codecourse.com>
2023-12-22 10:48:45 +0000

    Episode 12

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,12 +8,14 @@ use App\Models\Traits\HasReferrals;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Cashier\Billable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
     use HasReferrals;
+    use Billable;
 
     /**
      * The attributes that are mass assignable.
