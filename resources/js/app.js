1b6963468cf6cb2e654dda99074662dd6142070f
Alex Garrett-Smith <alex@codecourse.com>
2023-12-22 10:06:47 +0000

    Episode 08

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,10 @@
 import './bootstrap';
 
 import Alpine from 'alpinejs';
+import Clipboard from "@ryangjchandler/alpine-clipboard"
 
 window.Alpine = Alpine;
+
+Alpine.plugin(Clipboard)
 
 Alpine.start();
--- resources/views/referrals/partials/referral-code.blade.php
+++ resources/views/referrals/partials/referral-code.blade.php
@@ -9,8 +9,25 @@
         </p>
     </header>
 
-    <div class="mt-6 flex items-baseline space-x-3">
-        <x-text-input id="referral_code" type="text" value="{{ auth()->user()->referralLink() }}" class="mt-1 block w-full" readonly />
-        <button class="shrink-0 font-medium text-sm text-indigo-500">Copy link</button>
+    <div
+        class="mt-6 flex items-baseline space-x-3"
+        x-data="{
+            link: '{{ auth()->user()->referralLink() }}',
+            copied: false,
+            timeout: null,
+            copy () {
+                $clipboard(this.link)
+                this.copied = true
+
+                clearTimeout(this.timeout)
+
+                this.timeout = setTimeout(() => {
+                    this.copied = false
+                }, 3000)
+            }
+        }"
+    >
+        <x-text-input id="referral_code" type="text" value="{{ auth()->user()->referralLink() }}" class="mt-1 block w-full shrink-0" readonly />
+        <button class="shrink-0 font-medium text-sm text-indigo-500" x-on:click="copy" x-text="copied ? `Copied!` : `Copy link`">Copy link</button>
     </div>
 </section>
